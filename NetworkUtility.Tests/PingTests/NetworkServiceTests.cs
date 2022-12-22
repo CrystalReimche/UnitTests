5ba0bd398b1c9acc1d015442f6934fda398c9c1e
Crystal Reimche <36277207+CrystalReimche@users.noreply.github.com>
2022-12-22 13:22:37 -0600

    Added unit tests for DateTime, Object, and List of Objects

--- NetworkUtility.Tests/PingTests/NetworkServiceTests.cs
+++ NetworkUtility.Tests/PingTests/NetworkServiceTests.cs
@@ -1,20 +1,29 @@
 ﻿using FluentAssertions;
+using FluentAssertions.Extensions;
 using NetworkUtility.Ping;
+using System.Net.NetworkInformation;
 using Xunit;
 
 namespace NetworkUtility.Tests.PingTests
 {
     public class NetworkServiceTests
     {
-        // check out documentation (https://fluentassertions.com/strings/)
+        public readonly NetworkService networkService;
+
+        public NetworkServiceTests()
+        {
+            // SUT
+            networkService = new NetworkService();
+        }
+
+        // Check out documentation (https://fluentassertions.com/strings/)
         [Fact]
         public void NetworkService_SendPing_ReturnString()
         {
             // Arrange - variables, classes, mocks
-            var pingService = new NetworkService();
 
             // Act
-            var result = pingService.SendPing();
+            var result = networkService.SendPing();
 
             // Assert
             result.Should().NotBeNullOrWhiteSpace();
@@ -23,22 +32,76 @@ namespace NetworkUtility.Tests.PingTests
 
         }
 
-        // check out documentation (https://fluentassertions.com/numerictypes/)
+        // Check out documentation (https://fluentassertions.com/numerictypes/)
         [Theory]
         [InlineData(1, 1, 2)]
         [InlineData(2, 2, 4)]
         public void NetworkService_PingTimeout_ReturnInt(int a, int b, int expected)
         {
             // Arrange
-            var pingService = new NetworkService();
 
             // Act
-            var result = pingService.PingTimeout(a, b);
+            var result = networkService.PingTimeout(a, b);
 
             // Assert
             result.Should().Be(expected);
             result.Should().BeGreaterThanOrEqualTo(2);
             result.Should().NotBeInRange(-10000, 0);
+        }
+
+        // Check out documentation (https://fluentassertions.com/datetimespans/)
+        [Fact]
+        public void NetworkService_LastPingDate_ReturnDateTime()
+        {
+            // Arrange - variables, classes, mocks
+
+            // Act
+            var result = networkService.LastPingDate();
+
+            // Assert
+            result.Should().BeAfter(1.January(2010));
+            result.Should().BeBefore(1.January(2030));
+
+        }
+
+
+        [Fact]
+        public void NetworkService_GetPingOptions_ReturnObject()
+        {
+            // Arrange
+            var expected = new PingOptions()
+            {
+                DontFragment = true,
+                Ttl = 1
+            };
+
+            // Act
+            var result = networkService.GetPingOptions();
+
+            // Assert WARNING: "Be" careful
+            result.Should().BeOfType<PingOptions>();
+            result.Should().BeEquivalentTo(expected);
+            result.Ttl.Should().Be(1);
+        }
+
+
+        [Fact]
+        public void NetworkService_MostRecentPings_ReturnListOfObjects()
+        {
+            // Arrange
+            var expected = new PingOptions()
+            {
+                DontFragment = true,
+                Ttl = 1
+            };
+
+            // Act
+            var result = networkService.MostRecentPings();
+
+            // Assert WARNING: "Be" careful
+            result.Should().BeOfType<List<PingOptions>>();
+            result.Should().ContainEquivalentOf(expected);
+            result.Should().Contain(x => x.DontFragment == true);
         }
     }
 }
--- NetworkUtility/Ping/NetworkService.cs
+++ NetworkUtility/Ping/NetworkService.cs
@@ -1,4 +1,8 @@
-﻿namespace NetworkUtility.Ping
+﻿using System;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+
+namespace NetworkUtility.Ping
 {
     public class NetworkService
     {
@@ -10,6 +14,44 @@
         public int PingTimeout(int a, int b)
         {
             return a + b;
+        }
+
+        public DateTime LastPingDate()
+        {
+            return DateTime.Now;
+        }
+
+        public PingOptions GetPingOptions()
+        {
+            return new PingOptions()
+            {
+                DontFragment = true,
+                Ttl = 1,
+            };
+        }
+
+        public List<PingOptions> MostRecentPings()
+        {
+            List<PingOptions> pingOptions = new List<PingOptions>()
+            {
+                new PingOptions()
+                {
+                    DontFragment = true,
+                    Ttl = 1,
+                },
+                new PingOptions()
+                {
+                    DontFragment = true,
+                    Ttl = 1,
+                },
+                new PingOptions()
+                {
+                    DontFragment = true,
+                    Ttl = 1,
+                }
+            };
+            return pingOptions;
+
         }
     }
 }
